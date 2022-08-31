9f95d4482ff9ab8ad004d0ba45d174a9596e7dd2
shachem <sekki.hachem@outlook.com>
2022-08-31 02:44:22 +0100

    Use 'react' dynamic values to add title & link

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import "./App.css";
 
 function App() {
+  const title = "Welcome to my BLOG!";
+  const link = "https://www.google.com/";
   return (
     <div className="App">
       <div className="content">
-        <h1>App Component</h1>
+        <h1>{title}</h1>
+        <p>
+          <a href={link} target="blank">
+            Google Website
+          </a>
+        </p>
       </div>
     </div>
   );
