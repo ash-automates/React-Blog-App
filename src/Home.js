6d7b5096b4eb9d16a93de683b036dc236a7ff0fa
shachem <sekki.hachem@outlook.com>
2022-09-01 23:29:58 +0100

    Turn Fetch logic into custom hook for reusability

--- src/Home.js
+++ src/Home.js
@@ -1,35 +1,13 @@
-import { useState, useEffect } from "react";
 import BlogList from "./BlogList";
+import useFetch from "./useFetch";
 
 const Home = () => {
-  const [blogs, setBlogs] = useState(null);
-  const [isPending, setIsPending] = useState(true);
-  const [error, setError] = useState(null);
-
-  useEffect(() => {
-    fetch("http://localhost:8000/blogs")
-      .then((res) => {
-        if (!res.ok) {
-          throw new Error("Couldn't retrieve Blogs, try again later.");
-        }
-        return res.json();
-      })
-      .then((data) => {
-        setIsPending(false);
-        setBlogs(data);
-        setError(null);
-      })
-      .catch((error) => {
-        setIsPending(false);
-        setError(error.message);
-      });
-  }, []);
-
+  const { data, isPending, error } = useFetch("http://localhost:8000/blogs");
   return (
     <div className="home">
       {error && <h3>{error}</h3>}
       {isPending && <h3>Loading...</h3>}
-      {blogs && <BlogList blogs={blogs} title="All Blogs" />}
+      {data && <BlogList blogs={data} title="All Blogs" />}
     </div>
   );
 };
---
+++ src/useFetch.js
@@ -0,0 +1,30 @@
+import { useState, useEffect } from "react";
+
+const useFetch = (url) => {
+  const [data, setData] = useState(null);
+  const [isPending, setIsPending] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    fetch(url)
+      .then((res) => {
+        if (!res.ok) {
+          throw new Error("Couldn't retrieve Blogs, try again later.");
+        }
+        return res.json();
+      })
+      .then((data) => {
+        setIsPending(false);
+        setData(data);
+        setError(null);
+      })
+      .catch((error) => {
+        setIsPending(false);
+        setError(error.message);
+      });
+  }, [url]);
+
+  return { data, isPending, error };
+};
+
+export default useFetch;
