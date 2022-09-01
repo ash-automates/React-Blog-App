1aef48ba9ecf3058a297235b2768ee7522122ac1
shachem <sekki.hachem@outlook.com>
2022-09-01 11:49:49 +0100

    Handle errors if fetch is unsuccessful

--- src/Home.js
+++ src/Home.js
@@ -4,18 +4,30 @@ import BlogList from "./BlogList";
 const Home = () => {
   const [blogs, setBlogs] = useState(null);
   const [isPending, setIsPending] = useState(true);
+  const [error, setError] = useState(null);
 
   useEffect(() => {
     fetch("http://localhost:8000/blogs")
-      .then((res) => res.json())
+      .then((res) => {
+        if (!res.ok) {
+          throw new Error("Couldn't retrieve Blogs, try again later.");
+        }
+        return res.json();
+      })
       .then((data) => {
-        setBlogs(data);
         setIsPending(false);
+        setBlogs(data);
+        setError(null);
+      })
+      .catch((error) => {
+        setIsPending(false);
+        setError(error.message);
       });
   }, []);
 
   return (
     <div className="home">
+      {error && <h3>{error}</h3>}
       {isPending && <h3>Loading...</h3>}
       {blogs && <BlogList blogs={blogs} title="All Blogs" />}
     </div>
