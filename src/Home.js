ae153fff02e96fb0ef818d564b00fe2dded88002
shachem <sekki.hachem@outlook.com>
2022-09-01 08:51:18 +0100

    Fetch blogs from dummy API instead of local data

--- src/Home.js
+++ src/Home.js
@@ -1,38 +1,18 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import BlogList from "./BlogList";
 
 const Home = () => {
-  const [blogs, setBlogs] = useState([
-    {
-      title: "My new website",
-      body: "lorem ipsum...",
-      author: "Omar",
-      id: 1,
-    },
-    {
-      title: "Welcome party!",
-      body: "lorem ipsum...",
-      author: "Selim",
-      id: 2,
-    },
-    {
-      title: "Web dev top tips",
-      body: "lorem ipsum...",
-      author: "Omar",
-      id: 3,
-    },
-  ]);
+  const [blogs, setBlogs] = useState(null);
 
-  const handleDelete = (id) => {
-    const newBlogs = blogs.filter((blog) => {
-      return blog.id !== id;
-    });
-    setBlogs(newBlogs);
-  };
+  useEffect(() => {
+    fetch("http://localhost:8000/blogs")
+      .then((res) => res.json())
+      .then((data) => setBlogs(data));
+  }, []);
 
   return (
     <div className="home">
-      <BlogList blogs={blogs} title="All Blogs" handleDelete={handleDelete} />
+      {blogs && <BlogList blogs={blogs} title="All Blogs" />}
     </div>
   );
 };
